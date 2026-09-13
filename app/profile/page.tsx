aa8eb2fdca48b73878a96ad8fb7cc1be6a1c21e7
Oscar Öjling <oscarojling@gmail.com>
2026-09-13 12:28:14 +0200

    styling

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -11,7 +11,7 @@ const ProfilePage = () => {
       <div className="flex flex-col justify-between items-center mb-8 md:flex-row">
         <h1 className="font-display text-3xl md:text-4xl">Your Profile</h1>
         <button
-          className="cursor-pointer border border-black rounded-full px-4 py-2 mx-2 text-sm md:text-base hover:bg-primary hover:text-background hover:border-primary transition-colors"
+          className="cursor-pointer border border-black rounded-full px-5 py-3 mx-2 text-lg md:text-base hover:bg-primary hover:text-background hover:border-primary transition-colors"
           onClick={() => setUser(null)}
         >
           Log Out
